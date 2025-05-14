243587f74bb662ca8885c0e71ed237537893d22f
scosta18 <78115114+scosta18@users.noreply.github.com>
2025-05-13 20:19:21 -0400

    updated summerizer

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from summarizer import GeminiWebSummarizer
+from backend.src.summarizer import GeminiWebSummarizer
 
 import traceback
 
